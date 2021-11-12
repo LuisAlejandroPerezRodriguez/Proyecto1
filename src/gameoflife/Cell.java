6b6a20d993259bb9ba041b275893d696f6d48512
Alecosme <alejandrocosmediaz@gmail.com>
2021-11-12 13:27:43 -0400

    agregando todas las clases

---
+++ src/gameoflife/Cell.java
@@ -0,0 +1,29 @@
+
+package gameoflife;
+
+import java.awt.Color;
+
+public class Cell {
+    
+    private boolean celula; //atributos
+    
+      public Cell(boolean state){
+        this.celula=state;
+    }
+    
+    public boolean isAlive(){
+        
+     
+        return false;
+    }
+    
+    public void setState(boolean state){
+        //TODO implement me
+    } 
+    
+    public Color getColor(){
+      //TODO implement me
+       return null;
+    }
+    
+}
---
+++ src/gameoflife/GameOfLife.java
@@ -0,0 +1,10 @@
+
+package gameoflife;
+
+public class GameOfLife {
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
