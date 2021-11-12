b17e2c8b9f2ca84ce1314bf34fafa604a9f1c557
luis <luisalejandro.p.r@outlook.com>
2021-11-12 12:45:19 -0400

    Se agregran las clases

---
+++ src/gameoflife/Cell.java
@@ -0,0 +1,27 @@
+
+package gameoflife;
+
+public class Cell {
+    
+    private boolean celula; //atributos
+    
+      public Cell(boolean state){
+        this.celula=state;
+    }
+    
+    public boolean isAlive(){
+        
+     
+        return false;
+    }
+    
+    public void setState(boolean state){
+        //TODO implement me
+    } 
+    
+    public Color getColor(){
+      //TODO implement me
+       return null;
+    }
+    
+}
