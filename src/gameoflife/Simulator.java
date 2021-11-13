f068816062122d4685a7256040c07308016ac6f8
Alecosme <alejandrocosmediaz@gmail.com>
2021-11-12 23:41:11 -0400

    Diagramas UML Agregados

--- src/gameoflife/Simulator.java
+++ src/gameoflife/Simulator.java
@@ -10,5 +10,70 @@ package gameoflife;
  * @author luisa
  */
 public class Simulator {
+        //TODO implement me
     
+    
+    public boolean isRunning(){
+        //TODO implement me
+        return false;
+    }
+    
+    public void setRunning(){
+        //TODO implement me
+    }
+    
+     public void resetRunning(){
+        //TODO implement me
+    }
+    
+    public Cell getCell(int i, int j){
+       //TODO implement me
+        return null;
+    }
+    public int  getNumberOfSquares(){
+        //TODO implement me
+        return 0;
+    }
+    
+    public void randomGridCell(){
+        double n = Math.random();
+        //TODO implement me
+    }
+
+    void setCellState(int i, int j) {
+        
+       //TODO implement me
+    }
+//If the cell is alive in the current time step: If the cell has 2 or 3 living neighbors, then the cell 
+ //       remains alive in the next time step; otherwise, it dies.
+    //(A living cell dies of loneliness if it has 0 or 1 living neighbor, and of overcrowding if it has more than 3 living neighbors.)
+//If the cell is dead in the current time step: If the cell has 3 living neighbors, 
+    //then the cell becomes alive in the next time step; otherwise, it remains dead. 
+    //(Three living cells give birth to a new living cell.)
+    void update() {
+       //TODO implement me
+    }
+    int countAliveNeigbors(int x, int y){
+        //TODO implement me
+          return 0;
+    }
+    boolean  stayAlive(boolean isAlive, int x, int y){
+      
+       //TODO implement me
+         
+        
+        return false;
+    }
+    boolean isCellAlive(int x, int y){
+                    //TODO implement me
+                    return false;
+    }
+
+    void resetAll() {
+         //TODO implement me
+    }
+    public int getCountIteration(){
+        //TODO implement me
+        return 0;
+    }
 }
